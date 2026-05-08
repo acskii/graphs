4007bfc8a01c3990b059a1eecae2a9834eb9fa7e
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-05-08 14:04:54 +0300

    feat: add prim's algorithm + test case

--- main.py
+++ main.py
@@ -18,11 +18,21 @@
 #       Section: 1
 
 from sorting import topological_sort
+from prim import prims
 
 def main():
     # ADD TEST CASES HERE
-    
-    return
+    graph = {
+        0: [(1, 2), (3, 6)],
+        1: [(0, 2), (2, 3), (3, 8), (4, 5)],
+        2: [(1, 3), (4, 7)],
+        3: [(0, 6), (1, 8), (4, 9)],
+        4: [(1, 5), (2, 7), (3, 9)]
+    }
+
+    edges, weight = prims(5, graph, 0)
+    print(f"MST Edges: {edges}")
+    print(f"Total MST Weight: {weight}")
 
 if __name__ == "__main__":
     main()
